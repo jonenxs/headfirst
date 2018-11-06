3923ff5378c53bac65a606bc6536942824ae0cc8
聂孝爽 <xiaoshuang.nie@lanmaoly.com>
2018-11-06 14:14:35 +0800

    Initial commit

---
+++ src/main/java/com/nxs/factorypattern/Rectangle.java
@@ -0,0 +1,13 @@
+package com.nxs.factorypattern;
+
+/**
+ * @author 聂孝爽
+ * @date 2018/11/6 14:07
+ * @description
+ */
+public class Rectangle implements Shape {
+
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
---
+++ src/main/java/com/nxs/factorypattern/Square.java
@@ -0,0 +1,13 @@
+package com.nxs.factorypattern;
+
+/**
+ * @author 聂孝爽
+ * @date 2018/11/6 14:08
+ * @description
+ */
+public class Square implements Shape {
+
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
