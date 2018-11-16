003acab71098c5a0b3bf2c9c9febc4e32f046687
聂孝爽 <xiaoshuang.nie@lanmaoly.com>
2018-11-16 14:45:31 +0800

    原型模式

---
+++ src/main/java/com/nxs/prototypepattern/Circle.java
@@ -0,0 +1,18 @@
+package com.nxs.prototypepattern;
+
+/**
+ * @author 聂孝爽
+ * @date 2018/11/15 17:06
+ * @description
+ */
+public class Circle extends Shape {
+
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/main/java/com/nxs/prototypepattern/Shape.java
@@ -0,0 +1,42 @@
+package com.nxs.prototypepattern;
+
+/**
+ * @author 聂孝爽
+ * @date 2018/11/15 16:56
+ * @description
+ */
+public abstract class Shape implements Cloneable {
+
+    private String id;
+
+    protected String type;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public Object clone(){
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
